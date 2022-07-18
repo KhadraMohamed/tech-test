54ac2d684e7475bfd6ed1984d1e25bcfea2297cc
khadra mohamed <khadramohamed@live.co.uk>
2022-07-18 13:34:40 +0100

    SearchResults Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "../styles/app.css";
 import Search from "./Search";
+import SearchResults from "./SearchResults";
 
 const App = () => {
   const [searchResults, setSearchResults]= useState();
@@ -13,6 +14,7 @@ const App = () => {
       alt="nasaLogo"
       />
       <Search setSearchResults={setSearchResults}/>
+      <SearchResults />
     </div>
   );
 }
